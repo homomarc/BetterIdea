891aee89a635d4f0e39099b1a5e643da09cee547
Marc <shopping4f@googlemail.com>
2014-04-18 16:50:25 +0200

    -closetopic layout vorbereitung

--- BetterIdeaProject/src/com/betteridea/fragments/TopicCloseFragment.java
+++ BetterIdeaProject/src/com/betteridea/fragments/TopicCloseFragment.java
@@ -1,15 +1,22 @@
 package com.betteridea.fragments;
 
+import org.json.JSONArray;
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.betteridea.MainActivity;
 import com.betteridea.R;
+import com.betteridea.adapter.IdeaItemAdapter;
+import com.betteridea.connection.ServiceExecuter;
 import com.betteridea.models.TopicItem;
 
 public class TopicCloseFragment extends Fragment{
@@ -24,12 +31,25 @@ public class TopicCloseFragment extends Fragment{
  	View view = inflater.inflate(R.layout.topic_close_overview_layout, container, false);
  	 
  	ListView ideaList = (ListView) view.findViewById(R.id.list_topic_overview);
- 	 
+ 	TextView badgeCount = (TextView) view.findViewById(R.id.textBadgeCount);
+ 	MainActivity mainActivity = (MainActivity) getActivity();
+ 	IdeaItemAdapter adapter = mainActivity.getIdeaItemAdapter();
+ 	
+ 	JSONArray jsonArray = null;
+ 	
+ 	try{
+ 		String jsonString = new ServiceExecuter().execute("showTopic", new String(item.getTopicID()+"")).get();
+ 		jsonArray = new JSONArray(jsonString);
+ 		adapter = new IdeaItemAdapter(getActivity(),jsonArray,item);
+ 		ideaList.setAdapter(adapter);
+ 	}catch(Exception ex){
+ 		Log.v("test", "AsynchronException: " + ex.toString());
+ 	}
+ 	
+
+ 	mainActivity.setIdeaItemAdapter(adapter);
+ 	
  	return view;
  }
   
- @Override
- public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
- 	inflater.inflate(R.menu.topic, menu);
- }
 }
